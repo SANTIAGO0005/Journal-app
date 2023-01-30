61acec58ff377cdcc1e7b67e531d8f15bbaaa148
Santiago Cifuentes <cifuentess938@gmail.com>
2023-01-29 21:47:52 -0500

    first commit

---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,122 @@
+import { DeleteOutline, SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
+import { useRef } from "react";
+import { useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
+import { useForm } from "../../hooks/useForm";
+import { setActiveNote } from "../../store/journal/journalSlice";
+import { startDeletingNote, startSaveNote, startUploadingFiles } from "../../store/journal/thunks";
+import { ImageGallery } from "../components";
+
+export const NoteView = () => {
+  const dispatch = useDispatch();
+  const {
+    active: note,
+    messageSaved,
+    isSaving,
+  } = useSelector((state) => state.journal);
+
+  const { body, title, date, onInputChange, formState } = useForm(note);
+  const dateString = useMemo(() => {
+  const newDate = new Date(date);
+    return newDate.toUTCString();
+  }, [date]);
+
+  const fileInputRef = useRef();
+
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState]);
+
+  useEffect(() => {
+    if (messageSaved.length > 0) {
+      Swal.fire("Nota actualizada", messageSaved, "success");
+    }
+  }, [messageSaved]);
+
+  const onSaveNote = () => {
+    dispatch(startSaveNote());
+  };
+
+  const onFileInputChange = ({ target }) => {
+    if (target.files === 0) return;
+
+    dispatch( startUploadingFiles( target.files))
+  };
+
+  const onDelete = () => {
+     dispatch( startDeletingNote())
+  }
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      sx={{ mb: 1 }}
+      className="animate__animated animate__fadeIn animate__faster"
+    >
+      <Grid item>
+        <Typography fontSize={39} fontWeight="light">
+          {dateString}
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <input
+          type="file"
+          multiple
+          ref={fileInputRef}
+          onChange={onFileInputChange}
+          style={{ display: "none" }}
+        />
+
+        <IconButton color="primary" disabled={isSaving} onClick={ () => fileInputRef.current.click()}>
+          <UploadOutlined />
+        </IconButton>
+        <Button onClick={onSaveNote} disabled={isSaving}>
+          <SaveOutlined sx={{ padding: 2 }} />
+          Guardar
+        </Button>
+      </Grid>
+
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          placeholder="Ingrese un titulo"
+          label="Titulo"
+          sx={{ border: "none", mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
+        />
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="Que sucedio en el dia de hoy?"
+          minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
+        />
+      </Grid>
+
+      <Grid container justifyContent='end'>
+          <Button
+              onClick={ onDelete}
+              sx={{ mt: 2}}>
+            <DeleteOutline/>
+            Borrar
+          </Button>
+      </Grid>
+
+      <ImageGallery images={ note.imageUrls}/>
+    </Grid>
+  );
+};
